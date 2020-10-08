032ca2676def9daf415decff2072930003a429b4
Ash <ashley.bergsma42@gmail.com>
2020-10-08 11:45:04 -0700

    Spent some time getting to know Lists. List comprehensions are a tricky subject I need to get more familiar with.

--- src/04_printing.py
+++ src/04_printing.py
@@ -11,9 +11,9 @@ z = "I like turtles!"
 # Objective: # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
-# print("The string is %s" % z)
-# Walkthrough and Learn: #
 
+# Walkthrough and Learn: #
+print("The string is %s" % z)
 # The % is followed by s - for string 
 # after the closing bracket the % is referenced again, followed by the variable it's calling 
 # With the String Modulo Operator you control the output formatting! If you cast as a string (%s) the output will be a string. 
@@ -23,12 +23,28 @@ print("The string is %s" % x)
 # When we give more than one variable, regardless of type, at the end of the command we need to pass a Tuple containing the variables we want to substitute in the order that we want to substitute them 
 print("x is %d, the string is %s" % (x, z))
 # Now the % is followed by a d - for an integer(digit) - this will be a whole number 
-print("x is %(x)d, y is %(y)d, z is %(z)s")
+print("Key-Pairs Method: ","x is %(x)d, y is %(y)f, z is '%(z)s'" % {"x": x, "y": y, "z":z})
+# Key-Pairs Method: # 
+# To reference the variables by name within the format string (and avoid the possible confusion of order in a tuple) 
+# Using the f after % indicates the type is a Float 
+# Using this method, following the String Modulo Operator, you have to pass a dictionary of key-value pairs
+
 # Notes: 
 # also known as the String Modulo Operator 
 # This operator is 'overloaded' by the string class - thus its name. 
 # not in common use, but good to be familiar with if you run into legacy code 
 
 # Use the 'format' string method to print the same thing
+print("Format String Method: ","x is {}, y is {}, z is '{}'".format(x, y, z))
+# Format String Method: #
+# This method doesn't require any direct referencing in the string
+# The string is followed by the .format() method
+# We pass Positional Arguments into the format method. We can reference them by using their positional references, otherwise Python does them in order
+print("Changed the Positions: ","x is {1}, y is {2}, z is '{0}'".format(x, y, z))
+# The positional arguments are wrapped in a Tuple and can be directly re-arranged how we need them. 
 
 # Finally, print the same thing using an f-string
+print("F-String Method: ", f"x is {x}, y is {y}, and z is '{z}'")
+# F-String Method: #
+# This method is more current, and straightforward. You don't need to specify the datatypes, they're referenced right inside the Formatted String 
+# Python handles this under the hood? 
--- src/05_lists.py
+++ src/05_lists.py
@@ -8,22 +8,48 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
-print(x)
+
+x.append(4)
+
+print("Number One: ", x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
-print(x)
+
+x.extend(y)
+
+print("Number Two: ", x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
-print(x)
+
+x.pop(4)
+
+print("Number Three: ", x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
-print(x)
+
+x.extend([99, 100])
+
+print("Number Four: ", x)
 
 # Print the length of list x
 # YOUR CODE HERE
 
+print("Length of X is: ", len(x))
+
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+
+print("List Comprehension: ", [i * 1000 for i in x])
+
+# List Comprehensions: # 
+# Combine the creation or manipulation of an element and a for loop
+# Python automatically takes care of appending for you!
+# You can even add an IF at the end 
+
+print("If i > 4: ", [i * 1000 for i in x if i > 4])
+# i refers the original value of the list element.
+# so in this case only 9, 10, 99 and 100 were multiplied and returned 
+
