3889a54d1adc02774ac4b0f11668bd2d4c1b49e4
Ash <ashley.bergsma42@gmail.com>
2020-10-08 10:56:29 -0700

    Working through formatted strings, variables are becoming a better cemented concept as I must learn to pay attention to their TYPES

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -12,10 +12,22 @@ x = 5
 y = "7"
 
 # Write a print statement that combines x + y into the integer value 12
+# we need to typecast the string var y to a n integer using the int function 
+int_y = int(y)
+#after converting y add the result 
+result = x + int_y
+# or do it all in one line 
+one_line_result = int(y) + x 
 
-# YOUR CODE HERE
-
+print(result)
+print(one_line_result)
 
 # Write a print statement that combines x + y into the string value 57
+# now we need to do the opposite of above - instead of y being typcast into a integer, we must convert x to a string 
+str_x = str(x)
+string = str_x + y 
+# or do it in one line 
+one_line_string = str(x) + y 
 
-# YOUR CODE HERE
+print(string)
+print(one_line_string)
--- src/04_printing.py
+++ src/04_printing.py
@@ -8,9 +8,26 @@ x = 10
 y = 2.24552
 z = "I like turtles!"
 
-# Using the printf operator (%), print the following feeding in the values of x,
+# Objective: # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
+# print("The string is %s" % z)
+# Walkthrough and Learn: #
+
+# The % is followed by s - for string 
+# after the closing bracket the % is referenced again, followed by the variable it's calling 
+# With the String Modulo Operator you control the output formatting! If you cast as a string (%s) the output will be a string. 
+print("The string is %s" % x)
+# This shows that 10 was converted to a string and printed with the message. 
+# Referencing Multiple # 
+# When we give more than one variable, regardless of type, at the end of the command we need to pass a Tuple containing the variables we want to substitute in the order that we want to substitute them 
+print("x is %d, the string is %s" % (x, z))
+# Now the % is followed by a d - for an integer(digit) - this will be a whole number 
+print("x is %(x)d, y is %(y)d, z is %(z)s")
+# Notes: 
+# also known as the String Modulo Operator 
+# This operator is 'overloaded' by the string class - thus its name. 
+# not in common use, but good to be familiar with if you run into legacy code 
 
 # Use the 'format' string method to print the same thing
 
